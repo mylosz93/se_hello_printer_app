72fa466661da1f3cfdb8fffd8796e9b17b018746
mylosz93 <mylosz93@noreply.github.com>
2021-03-27 11:55:01 +0100

    dzialajace travis+heroku

--- test/test_views.py
+++ test/test_views.py
@@ -15,7 +15,7 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get("/?output=json")
-        self.assertEqual(b'{"imie": "Wiechu", "msg": "Hello World!"}', rv.data)
+        self.assertEqual(b'{"imie": "Milosz", "msg": "Hello World!"}', rv.data)
 
     def test_message_output_with_name(self):
         rv = self.app.get("/?output=json&name=Anna")
